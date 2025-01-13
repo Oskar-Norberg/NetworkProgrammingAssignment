96e134108cdf40d50c3705ade16919d48b31fc02
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-01-13 13:55:05 +0100

    Set up Camera Rotation

---
+++ Assets/Scripts/Player/Camera/PlayerCameraController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private PlayerMouseMovement mouseMovement;
+
+    [SerializeField] private float sensitivity = 1.0f;
+    
+    [SerializeField] private Transform playerRoot;
+    [SerializeField] private Transform cameraPivotTransform;
+
+    private void Update()
+    {
+        Vector2 mouseVector = mouseMovement.GetMouseMovement();
+        float y = sensitivity * mouseVector.x;
+        float x = sensitivity * -mouseVector.y;
+        
+        // Rotate player/parent around y-axis.
+        playerRoot.parent.Rotate(0.0f, y, 0.0f);
+        
+        // Rotate camera-pivot around x-axis.
+        cameraPivotTransform.Rotate(x, 0.0f, 0.0f);
+    }
+}
