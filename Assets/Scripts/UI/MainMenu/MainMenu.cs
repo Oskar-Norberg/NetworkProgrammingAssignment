0c088a2d20fe5ac2409e154f573de06a5d3d69e1
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-01-13 19:17:15 +0100

    Load Scene SampleScene on Host

--- Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -3,13 +3,17 @@ using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private string sceneName;
+    
     public void Host()
     {
         NetworkManager.Singleton.StartHost();
+        SceneManager.LoadScene(sceneName);
     }
 
     public void Join()
