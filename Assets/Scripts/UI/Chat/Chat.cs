490d83a8fd0fd60316c1acb891d8ed9656d54a24
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-01-14 17:38:47 +0100

    Create Chat script

---
+++ Assets/Scripts/UI/Chat/Chat.cs
@@ -0,0 +1,25 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class Chat : NetworkBehaviour
+{
+    [SerializeField] private GameObject message;
+    
+    [SerializeField] private GameObject messageBox;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SendMessageRpc(OwnerClientId + "", "test text");
+        }
+    }
+
+    [Rpc(SendTo.Everyone)]
+    private void SendMessageRpc(string messageName, string messageText)
+    {
+        var newMessage = Instantiate(message, messageBox.transform);
+        Message m = newMessage.GetComponent<Message>();
+        m.SetMessage(messageName, messageText);
+    }
+}
